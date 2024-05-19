04a7d007be9ec30fb3c866c7590eb5cb058ca817
Mikulash <62067494+mikulash@users.noreply.github.com>
2024-05-19 19:42:09 +0200

    facade response

---
+++ ExpensesManager.Server/Models/FacadeResponse.cs
@@ -0,0 +1,55 @@
+﻿namespace ExpensesManager.Server.Models;
+
+public class FacadeResponse<T>
+{
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
+    public bool IsSuccess => StatusCode is >= 200 and < 300;
+    public T? Value { get; set; }
+
+    public FacadeResponse<T> SetError(int statusCode, string message)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        return this;
+    }
+
+    public FacadeResponse<T> SetOk(T value)
+    {
+        StatusCode = 200;
+        Value = value;
+        return this;
+    }
+
+    public FacadeResponse<T> SetOk(int statusCode, T value)
+    {
+        if (statusCode is < 200 or >= 300)
+        {
+            throw new ArgumentException("Status code must be between 200 and 299", nameof(statusCode));
+        }
+        StatusCode = statusCode;
+        Value = value;
+        return this;
+    }
+
+    public FacadeResponse<T> SetBadRequest(string message)
+    {
+        StatusCode = 400;
+        Message = message;
+        return this;
+    }
+
+    public FacadeResponse<T> SetServerError(string message)
+    {
+        StatusCode = 500;
+        Message = message;
+        return this;
+    }
+
+    public FacadeResponse<T> SetNotFound(string message)
+    {
+        StatusCode = 404;
+        Message = message;
+        return this;
+    }
+}
