efe6374b95887c6efefbc6ed2c7373c197d11e3a
Mikulash <62067494+mikulash@users.noreply.github.com>
2024-05-19 19:38:14 +0200

    dotnet db update

---
+++ ExpensesManager.Server/Migrations/20240519173753_UpdateCategoryModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpensesManager.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCategoryModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Categories",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "CategoryId");
+        }
+    }
+}
