36b0a1892341b8961caef85f88163556f10ab70c
Mikulash <62067494+mikulash@users.noreply.github.com>
2024-06-07 14:38:44 +0200

    shadcn init

--- expensesmanager.client/src/App.tsx
+++ expensesmanager.client/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import {Button} from "./components/ui/button.tsx";
 
 function App() {
     return (
@@ -6,6 +7,7 @@ function App() {
             <h1 id="tabelLabel">Weather forecast</h1>
             <p>This component demonstrates fetching data from the server.</p>
             <div> heelo world</div>
+            <Button>Click me</Button>
         </div>
     );
 
